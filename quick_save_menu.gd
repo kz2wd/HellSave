37320882cd8144ef988e2ea0ebdc3c31bc265ffd
kz2wd <yrus.chan@gmail.com>
2025-01-24 16:06:53 +0100

    Added pull %

--- quick_save_menu.gd
+++ quick_save_menu.gd
@@ -11,7 +11,7 @@ var rng = RandomNumberGenerator.new()
 var respawn_position: Vector3
 
 func reset_menu() -> void:
-	current_percent = 100
+	current_percent = 0
 	respawn_position = world_spawn.position
 	
 func _ready() -> void:
@@ -40,17 +40,22 @@ func handle_respawn() -> void:
 	if not Input.is_action_just_pressed("respawn"): return
 	respawn()
 	
-func update_saving_luck_label() -> void:
-	$Saving_luck.text = str(current_percent) + " %"	
+func update_luck_label() -> void:
+	$Saving_luck.text = "Need : " + str(current_percent) + "%"
+
+func update_hit_label(pull: int) -> void:
+	$Saving_hit.text = "Pulled : " + str(pull) + "%"
 
 func try_save() -> void:
 	self.visible = true
 	$Saving_state.text = "SAVING"
-	update_saving_luck_label()
+	$Saving_hit.text = "pulling..."
+	update_luck_label()
 	await wait(2)
 	var pull = rng.randi_range(0, 100)
+	update_hit_label(pull)
 	# If fail
-	if pull > current_percent:
+	if pull < current_percent:
 		$Saving_state.text = "SAVING FAILED"
 		reset_menu()
 		await wait(1)
@@ -58,8 +63,8 @@ func try_save() -> void:
 		
 	else:
 		$Saving_state.text = "SAVED !"
-		current_percent -= 5
-		update_saving_luck_label()
+		current_percent += 5
+		update_luck_label()
 		respawn_position = player.position
 		await wait(1)
 	
